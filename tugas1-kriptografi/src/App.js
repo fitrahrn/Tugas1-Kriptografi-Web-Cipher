abcecf2014f21ffbdf143dd5c6e69fd89dc3c922
Fitrah Ramadhani Nugroho <fitnug30@gmail.com>
2024-02-17 11:34:32 +0700

    add variable state for saving form

--- tugas1-kriptografi/src/App.js
+++ tugas1-kriptografi/src/App.js
@@ -1,26 +1,44 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [textType,setType] = useState(""); //input type
+  const [inputText,setInput] = useState(""); //input text
+  const [cypherType,setCypher] = useState(""); //set cypher type
+  const [charType,setChar] = useState("")// plaintext or hex
+  const [cypherKey,setKey] = useState(""); // cipher key
+  const [resultText,setResult] = useState(""); //text after encrypted decrypt
+
+  const encrypt = async (event)=>{
+    event.preventDefault();
+    setResult(inputText);
+    console.log(textType);
+    console.log(cypherType);
+    console.log(charType);
+    console.log(cypherKey);
+  }
+
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={encrypt}>
         <label>Input Type: </label>
-        <select >
+        <select onChange={(event) =>setType(event.target.value)}>
           <option value="text">Text</option>
           <option value="file">File</option>
         </select>
         <label>Input Text: </label>
-        <input type="text"/><br/>
+        <input type="text" value={inputText} onInput={(event)=>setInput(event.target.value)}/>
         <div class="terms">
-          <input type="radio" id="plaintext" name="input_type" value="plaintext"/>
-          <label for="plaintext">Plaintext</label>
-          <input type="radio" id="hex" name="input_type" value="hex"/>
-          <label for="hex">Hex</label>
+          <input type="radio" id="plaintext" name="input_type" value="plaintext" onChange={(event)=>setChar(event.target.value)}/>
+          <label htmlFor="plaintext">Plaintext</label>
+          <input type="radio" id="hex" name="input_type" value="hex" onChange={(event)=>setChar(event.target.value)}/>
+          <label htmlFor="hex">Hex</label>
         </div>
         
         <label>Cipher Type: </label>
-        <select >
+        <select onChange={(event)=>setCypher(event.target.value)}>
           <option value="vignereStandard">Vignere Cipher Standard</option>
           <option value="autoKeyVignere">Auto-Key Vigenere</option>
           <option value="extendedVigenere">Extended Vigenere Cipher</option>
@@ -31,13 +49,13 @@ function App() {
           <option value="enigma">Enigma Cipher</option>
         </select>
         <label>Input Key: </label>
-        <input type="text"/>
-        <button>Encrypt</button>
-        <button>Decrypt</button>
+        <input type="text" onChange={(event)=>setKey(event.target.value)}/>
+        <button type="submit">Encrypt</button>
+        <button type="submit">Decrypt</button>
       </form>
       <div class="result">
       <label>Result Text</label>
-        <p value="encrypted"></p>
+        <p value={resultText}>{resultText}</p>
       </div>
         
 
